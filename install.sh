32aa3ec27aa25775d338fc868f92cdd07cd80773
abhishek sharma <abhisheksharma521020@gmail.com>
2022-11-09 23:49:44 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/p4nd4tz/zap.git "$HOME/.local/share/zap"
 
-echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh" >> $HOME/.config/zsh/.zshrc'
+echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> $HOME/.config/zsh/.zshrc
